8ddf98595d2c455ba51322ce5d893c1482fee686
Матюшин Никита <anick6850@gmail.com>
2026-02-27 06:03:24 +0300

    File updated at 2/27/2026, 6:03:23 AM

--- 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.js
+++ 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.js
@@ -1,32 +1,8 @@
-// Last updated: 2/27/2026, 4:01:10 AM
+// Last updated: 2/27/2026, 6:03:23 AM
 1/**
 2 * @param {string[]} word1
 3 * @param {string[]} word2
 4 * @return {boolean}
 5 */
-6var arrayStringsAreEqual = function(word1, word2) {
-7        let w1 = 0,
-8            w2 = 0; // Index of word
-9        let i = 0,
-10            j = 0; // Index of character
-11
-12        while (w1 < word1.length && w2 < word2.length) {
-13            if (word1[w1][i] !== word2[w2][j]) {
-14                return false;
-15            }
-16
-17            i++;
-18            j++;
-19
-20            if (i === word1[w1].length) {
-21                w1++;
-22                i = 0;
-23            }
-24            if (j === word2[w2].length) {
-25                w2++;
-26                j = 0;
-27            }
-28        }
-29        return w1 === word1.length && w2 === word2.length;
-30}
-31
+6var arrayStringsAreEqual = function(word1, word2) { let l = 0, r = 0; let i = 0, j = 0; while (i < word1.length && j < word2.length) { if (word1[i][l] !== word2[j][r]) return false; l++; r++; if (l === word1[i].length) { i++; l = 0; } if (r === word2[j].length) { j++; r = 0; } } return i === word1.length && j === word2.length; }
+7
