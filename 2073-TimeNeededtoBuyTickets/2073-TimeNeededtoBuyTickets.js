8f12d0f4d16cc35d7d44f42cc00943660c23f376
Матюшин Никита <anick6850@gmail.com>
2026-03-21 15:41:22 +0300

    File updated at 3/21/2026, 3:41:22 PM

--- 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.js
+++ 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.js
@@ -1,29 +1,21 @@
-// Last updated: 2/28/2026, 1:12:45 AM
+// Last updated: 3/21/2026, 3:41:22 PM
 1/**
 2 * @param {number[]} tickets
 3 * @param {number} k
 4 * @return {number}
 5 */
-6var timeRequiredToBuy = function(tickets, k) {
-7  let idx = 0;
-8  let time = 0;
+6function timeRequiredToBuy(tickets, k) {
+7  let i = 0;
+8  let count = 0;
 9
-10  while (true) {
-11    if (tickets[k] === 0) {
-12      return time;
-13    }
-14
-15    if (idx === tickets.length) {
-16      idx = 0;
-17    }
+10  while (tickets[k] !== 0){
+11    //пропускаем бедолаг
+12    while(tickets[i] === 0) i = (i + 1) % tickets.length;
+13    
+14    tickets[i]--;
+15    i = (i + 1) % tickets.length;
+16    count++;
+17  }
 18
-19    while (tickets[idx] === 0) {
-20      idx++
-21      idx = idx === tickets.length ? 0 : idx;
-22    }
-23
-24    tickets[idx]--
-25    idx++
-26    time++
-27  }
-28}
+19  return count;
+20}
