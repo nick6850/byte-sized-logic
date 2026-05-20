cff150c8e379173ca551274186c71967082f3161
Матюшин Никита <anick6850@gmail.com>
2026-05-20 03:24:04 +0300

    File updated at 5/20/2026, 3:24:03 AM

--- 2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes/2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes.js
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes/2962-CountSubarraysWhereMaxElementAppearsatLeastKTimes.js
@@ -1,18 +1,24 @@
-// Last updated: 5/9/2026, 6:06:57 PM
+// Last updated: 5/20/2026, 3:24:03 AM
 1function countSubarrays(nums, k) {
-2  const max = Math.max(...nums);
-3  let left = 0, maxCount = 0, result = 0;
-4
-5  for (let right = 0; right < nums.length; right++) {
-6    if (nums[right] === max) maxCount++;
-7
-8    while (maxCount === k) {
-9      if (nums[left] === max) maxCount--;
-10      left++;
-11    }
-12
-13    result += left;
-14  }
-15
-16  return result;
-17}
+2  let maxElement = 0;
+3
+4  for (const num of nums) if (num > maxElement) maxElement = num;
+5
+6  let maxCount = 0;
+7  let currCount = 0;
+8  let l = 0
+9
+10  for (let r = 0; r < nums.length; r++){
+11    if (nums[r] === maxElement){
+12      currCount++;
+13    }
+14
+15    while (currCount === k){
+16      maxCount += nums.length - r;
+17
+18      if (nums[l++] === maxElement) currCount--;
+19    }
+20  }
+21
+22  return maxCount;
+23}
