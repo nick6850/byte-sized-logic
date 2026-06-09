df419278d0d1f797f3330d801eb7f7b3217b7c02
Матюшин Никита <anick6850@gmail.com>
2026-06-09 05:09:38 +0300

    File updated at 6/9/2026, 5:09:38 AM

--- 2461-MaximumSumofDistinctSubarraysWithLengthK/2461-MaximumSumofDistinctSubarraysWithLengthK.js
+++ 2461-MaximumSumofDistinctSubarraysWithLengthK/2461-MaximumSumofDistinctSubarraysWithLengthK.js
@@ -1,26 +1,24 @@
-// Last updated: 5/18/2026, 4:04:11 AM
+// Last updated: 6/9/2026, 5:09:38 AM
 1function maximumSubarraySum(nums, k) {
 2  let maxSum = 0;
-3  let currSum = 0;
+3  let sum = 0;
 4  const window = new Set();
-5  let l = 0;
+5  let l = 0
 6
 7  for (let r = 0; r < nums.length; r++) {
-8    while (window.has(nums[r])) {
-9      currSum -= nums[l];
-10      window.delete(nums[l++])
-11    }
-12
-13    currSum += nums[r];
-14    window.add(nums[r]);
-15
-16    if (window.size > k) {
-17      currSum -= nums[l];
-18      window.delete(nums[l++])
+8    while (window.has(nums[r]) || window.size === k) {
+9      window.delete(nums[l])
+10      sum -= nums[l];
+11      l++
+12    }
+13
+14    sum += nums[r];
+15    window.add(nums[r]);
+16
+17    if(window.size === k){
+18      maxSum = Math.max(maxSum, sum);
 19    }
-20
-21    if (window.size >= k) maxSum = Math.max(maxSum, currSum)
-22  }
-23
-24  return maxSum;
-25}
+20  }
+21
+22  return maxSum;
+23}
