142bcbc4dd502a1f2cca3d1876414e5c32d15ca1
Матюшин Никита <anick6850@gmail.com>
2026-06-14 18:03:34 +0300

    File updated at 6/14/2026, 6:03:33 PM

--- 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.js
+++ 1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit/1438-LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.js
@@ -1,38 +1,34 @@
-// Last updated: 6/4/2026, 2:45:14 AM
+// Last updated: 6/14/2026, 6:03:33 PM
 1function longestSubarray(nums, limit) {
-2  const max_deque = [];
-3  const min_deque = [];
+2  const minDeque = [];
+3  const maxDeque = [];
 4
-5  let l = 0
-6  let longest = 0;
+5  let left = 0
+6  let result = 0;
 7
-8  for (let r = 0; r < nums.length; r++) {
-9    const num = nums[r];
-10
-11    while (min_deque.length && nums[min_deque.at(-1)] >= num) {
-12      //evict older (no use)
-13      min_deque.pop();
-14    }
-15
-16    while (max_deque && nums[max_deque.at(-1)] <= num) {
-17      //evict older (no use)
-18      max_deque.pop();
-19    }
-20
-21    min_deque.push(r);
-22    max_deque.push(r);
-23
-24    
-25    while (Math.abs(nums[max_deque[0]] - nums[min_deque[0]]) > limit){
-26      if (max_deque[0] < min_deque[0]) {
-27        l = max_deque.shift() + 1
-28      } else {
-29        l = min_deque.shift() + 1;
-30      }
-31    }
-32
-33    longest = Math.max(longest, r-l+1);
-34  }
-35
-36  return longest;
-37}
+8  for (let right = 0; right < nums.length; right++) {
+9    //evict smaller
+10    while (maxDeque.length && nums[maxDeque.at(-1)] <= nums[right]) {
+11      maxDeque.pop();
+12    }
+13
+14    //evict bigger
+15    while (minDeque.length && nums[minDeque.at(-1)] >= nums[right]) {
+16      minDeque.pop();
+17    }
+18
+19    minDeque.push(right)
+20    maxDeque.push(right)
+21
+22    //at this point we have the biggest and smallest values
+23    while (maxDeque.length && minDeque.length && (nums[maxDeque[0]] - nums[minDeque[0]] > limit)) {
+24      if (maxDeque[0] <= left) maxDeque.shift();
+25      if (minDeque[0] <= left) minDeque.shift();
+26      left++
+27    }
+28
+29    result = Math.max(result, right - left + 1)
+30  }
+31
+32  return result;
+33}
